932b573e4cb78871c9cbb5b8c0f5c605dad21972
GarBear121 <43530387+GarBear121@users.noreply.github.com>
2022-09-19 21:30:46 -0400

    Add files via upload

--- scripts/combat/script_warlock.lua
+++ scripts/combat/script_warlock.lua
@@ -325,7 +325,7 @@ function script_warlock:run(targetGUID)
 			JumpOrAscendStart();
 		end
 
-		if (targetObj:IsInLineOfSight() and not IsMoving()) then
+		if (targetObj:IsInLineOfSight() and not IsMoving()) and (targetObj:GetHealthPercentage() < 99) then
 			if (targetObj:GetDistance() <= self.followTargetDistance) and (targetObj:IsInLineOfSight()) then
 				if (not targetObj:FaceTarget()) then
 					targetObj:FaceTarget();
