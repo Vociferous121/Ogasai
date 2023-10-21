271aa676b560b08c5a1bf2516a67df29b367e3b8
Vociferous <43530387+Vociferous121@users.noreply.github.com>
2023-10-21 16:22:10 -0400

    Add files via upload

--- scripts/followerHeals/script_priestFollowerHeals.lua
+++ scripts/followerHeals/script_priestFollowerHeals.lua
@@ -34,7 +34,7 @@ function script_priestFollowerHeals:HealsAndBuffs()
 			partyMember = GetLocalPlayer();
 		end
 		local partyMembersHP = partyMember:GetHealthPercentage();
-		if (partyMembersHP > 0 and partyMembersHP < 99 and localMana > 1) then
+		if (partyMembersHP > 0 and localMana > 1) then
 			local partyMemberDistance = partyMember:GetDistance();
 			leaderObj = GetPartyMember(GetPartyLeaderIndex());
 			local localHealth = GetLocalPlayer():GetHealthPercentage();					
--- scripts/script_follow.lua
+++ scripts/script_follow.lua
@@ -54,13 +54,13 @@ function getPartyMembers()
 		if (i == GetNumPartyMembers()+1) then
 			partyMember = GetLocalPlayer();
 		end
-		local partyMembersHP = partyMember:GetHealthPercentage();
-		if (GetNumPartyMembers() >= 1) then
-			if (partyMembersHP > 0 and partyMembersHP < 99 and localMana > 1) then
+		local localMana = GetLocalPlayer():GetManaPercentage();
+		local localEnergy = GetLocalPlayer():GetEnergyPercentage();
+		local partyMemberHP = partyMember:GetHealthPercentage();
+		if (partyMemberHP > 0) and (localMana > 1 or localEnergy > 1) then
 				local partyMemberDistance = partyMember:GetDistance();
 				leaderObj = GetPartyMember(GetPartyLeaderIndex());
 				local localHealth = GetLocalPlayer():GetHealthPercentage();
-			end
 		end
 	end
 end
