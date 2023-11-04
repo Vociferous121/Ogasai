38efc4367872d1664b6c7b078797568b0fe3014b
shikulja <gamemazahist@gmail.com>
2023-11-05 00:45:38 +0300

    Includibg libs and checking them

--- core/coremenu.lua
+++ core/coremenu.lua
@@ -1,140 +1,185 @@
+----------------------------
+---------Load Libs----------
+----------------------------
+include("libs\\AceLibrary\\AceLibrary.lua")
+include("libs\\AceLocale-2.2\\AceLocale-2.2.lua")
+--include("libs\\Babble-Race-2.2\\Babble-Race-2.2.lua")
+
+include("libs\\LibStub\\LibStub.lua")
+
+include("libs\\CallbackHandler-1.0\\CallbackHandler-1.0.lua")
+
+--include("libs\\AceCore-3.0\\AceCore-3.0.lua")
+--include("libs\\AceDB-3.0\\AceDB-3.0.lua")
+--include("libs\\AceDBOptions-3.0\\AceDBOptions-3.0.lua")
+
+include("libs\\AceLocale-3.0\\AceLocale-3.0.lua")
+include("libs\\AceLocale-3.1\\AceLocale-3.1.lua")
+
+--include("libs\\LibBabble-Class-3.0\\LibBabble-3.0.lua")
+--include("libs\\LibBabble-Class-3.0\\LibBabble-Class-3.0.lua")
+
+include("libs\\LibBabble-Spell-3.0\\LibBabble-3.0.lua")
+include("libs\\LibBabble-Spell-3.0\\LibBabble-Spell-3.0.lua")
+include("libs\\LibBabble-Spell-3.0\\deDE.lua")
+include("libs\\LibBabble-Spell-3.0\\esES.lua")
+include("libs\\LibBabble-Spell-3.0\\frFR.lua")
+include("libs\\LibBabble-Spell-3.0\\koKR.lua")
+include("libs\\LibBabble-Spell-3.0\\zhCN.lua")
+include("libs\\LibBabble-Spell-3.0\\zhTW.lua")
+include("libs\\LibBabble-Spell-3.0\\ruRU.lua")
+include("libs\\LibBabble-Spell-3.0\\Cleanup.lua")
+
+----------------------------
+-----Load Localization------
+----------------------------
+include("locales\\Locale_enUS.lua")
+include("locales\\Locale_ruRU.lua")
+
+----------------------------
+---------Local--------------
+----------------------------
+local L = LibStub("AceLocale-3.0"):GetLocale("oGasai")
+--local BC = AceLibrary("Babble-Class-2.2")
+--local BC = LibStub("LibBabble-Class-3.0")
+
 coremenu = {
 	--Setup
 	isSetup = false,
 }
 
 function coremenu:reload()
-	self.isSetup = false;
-	coremenu:draw();
+	self.isSetup = false
+	coremenu:draw()
 end
 
 function coremenu:draw()
+	if self.isSetup == false then
+		self.isSetup = true
 
-	if (self.isSetup == false) then
-		
-		self.isSetup = true;
-		
-		DEFAULT_CHAT_FRAME:AddMessage('Loading Scripts!');
-		
+		DEFAULT_CHAT_FRAME:AddMessage(L["Loading Scripts!"])
+
 		--[[
 			----------------------------
 			Core Files
 			----------------------------
-		]]--
-		
-		include("core\\core.lua");	
-        
-		-- Load DBs
-		include("scripts\\db\\vendorDB.lua");
-		include("scripts\\db\\hotspotDB.lua");
+		]]
+		--
 
+		include("core\\core.lua")
+
+		-- Load DBs
+		include("scripts\\db\\vendorDB.lua")
+		include("scripts\\db\\hotspotDB.lua")
 
 		--[[
 			----------------------------
 			Class Rotations
 			----------------------------
-		]]--
-		
-		LoadScript("Mage", "scripts\\combat\\script_mage.lua");
-		AddScriptToCombat("Mage", "script_mage");
+		]]
+		--
 
-		LoadScript("Rogue", "scripts\\combat\\script_rogue.lua");
-		AddScriptToCombat("Rogue", "script_rogue");
+		LoadScript("Mage", "scripts\\combat\\script_mage.lua")
+		AddScriptToCombat(L["Mage"], "script_mage")
 
-		LoadScript("Hunter", "scripts\\combat\\script_hunter.lua");
-		AddScriptToCombat("Hunter", "script_hunter");
-		
-		LoadScript("Warlock", "scripts\\combat\\script_warlock.lua");
-		AddScriptToCombat("Warlock", "script_warlock");
+		LoadScript("Rogue", "scripts\\combat\\script_rogue.lua")
+		AddScriptToCombat(L["Rogue"], "script_rogue")
 
-		LoadScript("Warrior", "scripts\\combat\\script_warrior.lua");
-		AddScriptToCombat("Warrior", "script_warrior");
+		LoadScript("Hunter", "scripts\\combat\\script_hunter.lua")
+		AddScriptToCombat(L["Hunter"], "script_hunter")
 
-		LoadScript("Paladin", "scripts\\combat\\script_paladin.lua");
-		AddScriptToCombat("Paladin", "script_paladin");
+		LoadScript("Warlock", "scripts\\combat\\script_warlock.lua")
+		AddScriptToCombat(L["Warlock"], "script_warlock")
 
-		LoadScript("Priest", "scripts\\combat\\script_priest.lua");
-		AddScriptToCombat("Priest", "script_priest");
+		LoadScript("Warrior", "scripts\\combat\\script_warrior.lua")
+		AddScriptToCombat(L["Warrior"], "script_warrior")
 
-		LoadScript("Shaman", "scripts\\combat\\script_shaman.lua");
-		AddScriptToCombat("Enhance - Shaman", "script_shaman");
+		LoadScript("Paladin", "scripts\\combat\\script_paladin.lua")
+		AddScriptToCombat(L["Paladin"], "script_paladin")
 
-		LoadScript("Druid", "scripts\\combat\\script_druid.lua");
-		AddScriptToCombat("Druid", "script_druid");
+		LoadScript("Priest", "scripts\\combat\\script_priest.lua")
+		AddScriptToCombat(L["Priest"], "script_priest")
+
+		LoadScript("Shaman", "scripts\\combat\\script_shaman.lua")
+		AddScriptToCombat(L["Enhance - Shaman"], "script_shaman")
+
+		LoadScript("Druid", "scripts\\combat\\script_druid.lua")
+		AddScriptToCombat(L["Druid"], "script_druid")
 
 		--[[
 			----------------------------
 			Bot Types
 			----------------------------
-		]]--
-	
-		LoadScript("Grinder", "scripts\\script_grind.lua");
-		AddScriptToMode("Grinder", "script_grind");
+		]]
+		--
 
-		LoadScript("Follower", "scripts\\script_follow.lua");
-		AddScriptToMode("Follower", "script_follow");
+		LoadScript(L["Grinder"], "scripts\\script_grind.lua")
+		AddScriptToMode(L["Grinder"], "script_grind")
 
-		LoadScript("Rotation", "scripts\\script_rotation.lua");
-		AddScriptToMode("Rotation", "script_rotation");
+		LoadScript(L["Follower"], "scripts\\script_follow.lua")
+		AddScriptToMode(L["Follower"], "script_follow")
 
-		LoadScript("Fishing", "scripts\\script_fish.lua");
-		AddScriptToMode("Fishing", "script_fish");
+		LoadScript(L["Rotation"], "scripts\\script_rotation.lua")
+		AddScriptToMode(L["Rotation"], "script_rotation")
+
+		LoadScript(L["Fishing"], "scripts\\script_fish.lua")
+		AddScriptToMode(L["Fishing"], "script_fish")
 
 		-- Nav Mesh Runner by Rot, Improved by Logitech
-		LoadScript("Runner", "scripts\\script_runner.lua");
-		AddScriptToMode("Runner", "script_runner");
+		LoadScript(L["Runner"], "scripts\\script_runner.lua")
+		AddScriptToMode(L["Runner"], "script_runner")
 
 		--LoadScript("Unstuck Test", "scripts\\script_unstuck.lua");
 		--AddScriptToMode("Unstuck Test", "script_unstuck");
 
 		--LoadScript("Pather", "scripts\\script_pather.lua");
 		--AddScriptToMode("Pather Debug", "script_pather");
-		
+
 		--[[
 			----------------------------
 			Override Settings
 			----------------------------
-		]]--
-	
-		DrawPath(true);
-		
+		]]
+		--
+
+		DrawPath(true)
+
 		--NewTheme(false);
-		
 	end
 
 	--[[
 		----------------------------
 		Append To Menu
 		----------------------------
-	]]--
+	]]
+	--
 
-	-- Grind 
-	Separator();
-	if (CollapsingHeader("Grind options")) then
-		script_grindMenu:menu();
+	-- Grind
+	Separator()
+	if CollapsingHeader(L["Grind options"]) then
+		script_grindMenu:menu()
 	end
 
-	if (CollapsingHeader("Follower options")) then
-		script_followEX:menu();
+	if CollapsingHeader(L["Follower options"]) then
+		script_followEX:menu()
 	end
 
-	if (CollapsingHeader("Fishing options")) then
-		script_fish:menu();
+	if CollapsingHeader(L["Fishing options"]) then
+		script_fish:menu()
 	end
-	
-	Separator();
+
+	Separator()
 
 	-- Add Combat scripts menus
-	if (CollapsingHeader("Combat options")) then
-		script_mageEX:menu();
-		script_hunterEX:menu();
-		script_warlockEX:menu();
-		script_paladinEX:menu();
-		script_druidEX:menu();
-		script_priestEX:menu();
-		script_warriorEX:menu();
-		script_rogueEX:menu();
-		script_shamanEX:menu();
+	if CollapsingHeader(L["Combat options"]) then
+		script_mageEX:menu()
+		script_hunterEX:menu()
+		script_warlockEX:menu()
+		script_paladinEX:menu()
+		script_druidEX:menu()
+		script_priestEX:menu()
+		script_warriorEX:menu()
+		script_rogueEX:menu()
+		script_shamanEX:menu()
 	end
-	
 end
