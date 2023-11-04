815bd73295c8b81df0155763f2491c76ac577069
shikulja <gamemazahist@gmail.com>
2023-11-05 00:47:01 +0300

    Adding a translation project
    A minimal example to work in the future

---
+++ locales/Locale_enUS.lua
@@ -0,0 +1,40 @@
+local L = LibStub("AceLocale-3.0"):NewLocale("oGasai", "enUS", true, true)
+
+--[[
+
+What need check leater:
+--coremenu
+
+pls check:
+UnitClass
+UnitRace
+UnitFactionGroup
+
+--]]
+
+-- English localization
+
+-- CORE --
+L["Loading Scripts!"] = true
+
+--Class Rotations NAMES (NOT CLASES)
+L["Mage"] = true
+L["Rogue"] = true
+L["Hunter"] = true
+L["Warlock"] = true
+L["Warrior"] = true
+L["Paladin"] = true
+L["Priest"] = true
+L["Enhance - Shaman"] = true
+L["Druid"] = true
+
+-- deduplication
+L["Grinder"] = true
+L["Follower"] = true
+L["Rotation"] = true
+L["Fishing"] = true
+L["Runner"] = true
+L["Grind options"] = true
+L["Follower options"] = true
+L["Fishing options"] = true
+L["Combat options"] = true
---
+++ locales/Locale_ruRU.lua
@@ -0,0 +1,33 @@
+local L = LibStub("AceLocale-3.0"):NewLocale("oGasai", "ruRU", false)
+if not L then
+	return
+end
+
+-- Russian localization
+if L then
+	-- CORE --
+	L["Loading Scripts!"] = "Загрузка скриптов!"
+
+	--Class Rotations NAMES (NOT CLASES)
+	L["Mage"] = nil
+	L["Rogue"] = nil
+	L["Hunter"] = nil
+	L["Warlock"] = nil
+	L["Warrior"] = nil
+	L["Paladin"] = nil
+	L["Priest"] = nil
+	L["Enhance - Shaman"] = nil
+	L["Druid"] = nil
+
+	-- deduplication
+	L["Grinder"] = nil --"Гриндер"
+	L["Follower"] = nil --"Следователь"
+	L["Rotation"] = nil --"Ротация"
+	L["Fishing"] = nil --"Рыбалка"
+	L["Runner"] = nil --"Пробежка"
+	L["Grind options"] = nil --"Настройки гринда"
+	L["Follower options"] = nil --"Настройки следования"
+	L["Fishing options"] = nil --"Настройки рыбалки"
+	L["Combat options"] = nil --"Настройки боя"
+
+end
