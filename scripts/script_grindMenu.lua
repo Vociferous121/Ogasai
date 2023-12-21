f89f4f6ff01e901d36a04d5890133c4e6acbd8e4
Vociferous <43530387+Vociferous121@users.noreply.github.com>
2023-12-20 23:45:15 -0500

    Add files via upload

--- scripts/script_grindMenu.lua
+++ scripts/script_grindMenu.lua
@@ -39,8 +39,8 @@ function script_grindMenu:menu()
 	end
 
 	--garbage collection info
-	local a = gcinfo();
-	Text("Garbage Data Lost " ..a);
+	--local a = gcinfo();
+	--Text("Garbage Data Lost " ..a);
 
 	if (GetXPExhaustion() ~= nil) and (not script_paranoia.paranoiaUsed) then
 		if (math.ceil(20*GetXPExhaustion()/UnitXPMax("player")) == 30) then
